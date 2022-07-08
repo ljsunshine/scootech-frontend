5d24a2656bc19d0df71730affbd8659549e44616
lisa.dyllong <lisa.dyllong@xing.com>
2022-07-08 12:36:51 +0200

    more adjustments

--- scripts.js
+++ scripts.js
@@ -1,17 +1,13 @@
-var request = new XMLHttpRequest()
+const thisForm = document.getElementById('calcuation');
+thisForm.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const formData = new FormData(thisForm).entries()
+    const response = await fetch('http://85.214.16.235:8080', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(Object.fromEntries(formData))
+    });
 
-request.open('POST', 'http://85.214.16.235:8080/s', true)
-request.onload = function() {
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response)
-
-    if (request.status >= 200 && request.status < 400) {
-        data.forEach(movie => {
-            console.log(movie.title)
-        })
-    } else {
-        console.log('error')
-    }
-}
-
-request.send()
+    const result = await response.json();
+    console.log(result)
+})
